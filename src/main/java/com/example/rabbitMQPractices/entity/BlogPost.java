1786344b1b99bb553f5130f2e2226a6ffe9a4e59
drahmetacik <a.acik@icloud.com>
2025-04-09 22:50:59 +0100

    initialize RabbitMQ practices project with Maven and Spring Boot

---
+++ src/main/java/com/example/rabbitMQPractices/entity/BlogPost.java
@@ -0,0 +1,51 @@
+package com.example.rabbitMQPractices.entity;
+
+import java.io.Serializable;
+
+public class BlogPost implements Serializable {
+    private String id;
+    private String title;
+    private String content;
+
+    public BlogPost() {
+    }
+
+    public BlogPost(String id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "BlogPost{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/example/rabbitMQPractices/RabbitMqPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitMQPractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
