517243018d5022cea39268c2bd237024a65a60ce
drahmetacik <a.acik@icloud.com>
2025-04-10 00:11:52 +0100

    add test producer method to RabbitMqPracticesApplication for sending BlogPost messages

--- src/main/java/com/example/rabbitMQPractices/RabbitMqPracticesApplication.java
+++ src/main/java/com/example/rabbitMQPractices/RabbitMqPracticesApplication.java
@@ -1,13 +1,24 @@
 package com.example.rabbitMQPractices;
 
+import com.example.rabbitMQPractices.entity.BlogPost;
+import com.example.rabbitMQPractices.producer.BlogPostProducer;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RabbitMqPracticesApplication {
 
+	@Autowired
+	private BlogPostProducer blogPostProducer;
+
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitMqPracticesApplication.class, args);
 	}
-
+	@PostConstruct
+	public void testProducer() {
+		BlogPost blogPost = new BlogPost("1", "Test Title", "Test Content");
+		blogPostProducer.sendBlogPost(blogPost);
+	}
 }
